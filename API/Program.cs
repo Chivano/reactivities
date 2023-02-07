a9ca3380d70d5990f42c2d421a7466f32bdb0ee9
Tobias Johansson <tobias_johansson@live.se>
2023-02-07 19:16:18 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => {
+builder.Services.AddDbContext<DataContext>(opt =>
+{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt => opt.AddPolicy("CorsPolicy", policy =>
+{
+    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+}));
 
 
 var app = builder.Build();
@@ -25,7 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 
-
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
@@ -33,12 +38,15 @@ app.MapControllers();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
-try {
+try
+{
     var context = services.GetRequiredService<DataContext>();
     await context.Database.MigrateAsync();
     await Seed.SeedData(context);
-} catch(Exception e){
-    var logger= services.GetRequiredService<ILogger<Program>>();
+}
+catch (Exception e)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(e, "An error occured during migration");
 }
 
